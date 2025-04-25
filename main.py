2c4c901fb3ccdb4ff8c02cd3e1f2b760bd32c32d
24Ton <58786874+BlauerTon@users.noreply.github.com>
2025-04-25 13:31:38 +0300

    methods to call class attributes

---
+++ main.py
@@ -0,0 +1,15 @@
+from pet import Pet
+
+buddy = Pet("Buddy")
+
+
+buddy.eat()
+buddy.sleep()
+buddy.play()
+buddy.train("Sit")
+buddy.train("Stay")
+buddy.train("Roll Over")
+
+buddy.get_status()
+
+buddy.show_tricks()
